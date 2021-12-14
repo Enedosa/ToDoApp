72a70d5edbee1f83ee491168367d4515aca4ac68
Usman Kulaha <32143087+UK818@users.noreply.github.com>
2021-12-14 21:11:53 +0100

    Tuesday commit

---
+++ ToDo/ToDo/MainViewController.swift
@@ -0,0 +1,111 @@
+//
+//  MainViewController.swift
+//  ToDo
+//
+//  Created by mac on 13/12/2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+	
+	@IBOutlet weak var welcomeMsg: UILabel!
+	
+	@IBOutlet weak var addTodoButton: UIButton!
+	
+	@IBOutlet weak var todoListTableView: UITableView!
+	
+	var todos = [String]()
+	
+	var name = ""
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+		
+		todoListTableView?.dataSource = self
+		todoListTableView?.delegate = self
+		
+		todos = UserDefaults.standard.stringArray(forKey: "todos") ?? []
+		name = UserDefaults.standard.string(forKey: "name") ?? ""
+		
+		addTodoButton?.addTarget(self, action: #selector(didTapAdd), for: .touchUpInside)
+		welcomeMsg.text = "Hello \(name)!"
+		
+	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		
+		todoConfig()
+		todoListTableView?.reloadData()
+	}
+	
+	private func todoConfig() {
+		todos = UserDefaults.standard.array(forKey: "todos") as? [String] ?? []
+		UserDefaults.resetStandardUserDefaults()
+	}
+	
+	@objc func didTapAdd() {
+		let createVC = CreateViewController()
+		createVC.addTodos = { [weak self] newTodo in
+			self?.todos.append(newTodo)
+			print(self?.todos ?? [])
+			UserDefaults.standard.set(self?.todos, forKey: "todos")
+			UserDefaults.resetStandardUserDefaults()
+			self?.todoListTableView.reloadData()
+		}
+		navigationController?.pushViewController(createVC, animated: true)
+	}
+	
+	@objc func didTapDelete() {
+		UserDefaults.standard.removeObject(forKey: "todos")
+		UserDefaults.resetStandardUserDefaults()
+		todoListTableView.reloadData()
+		print(todos)
+	}
+	
+}
+
+extension MainViewController: UITableViewDataSource {
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		todos.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		
+		guard let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath) as? TodoTableViewCell else {
+			fatalError("unable to dequeue")
+		}
+		cell.todoLabel.text = todos[indexPath.row]
+		cell.didDelete = { [weak self] in
+			guard let self = self else { return }
+			self.todos.remove(at: indexPath.row)
+			UserDefaults.standard.set(self.todos, forKey: "todos")
+			UserDefaults.resetStandardUserDefaults()
+			self.todoListTableView.reloadData()
+	    }
+		
+		return cell
+	}
+	
+}
+
+extension MainViewController: UITableViewDelegate {
+	
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		tableView.deselectRow(at: indexPath, animated: true)
+		let createVC = CreateViewController()
+		createVC.textFieldText = todos[indexPath.row]
+		createVC.btnTitle = "Update Todo"
+		createVC.addTodos = { [weak self] newTodo in
+			self?.todos[indexPath.row] = newTodo
+			print(self?.todos ?? [])
+			UserDefaults.standard.set(self?.todos, forKey: "todos")
+			UserDefaults.resetStandardUserDefaults()
+			self?.todoListTableView.reloadData()
+		}
+		navigationController?.pushViewController(createVC, animated: true)
+	}
+	
+}
